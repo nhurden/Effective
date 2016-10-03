023b3cbc4efdc4ccc140e7890ef3e4702856aac4
Nicholas Hurden <git@nhurden.com>
2016-10-03 14:41:59 +1100

    Add a debug interceptor

--- SwiftFrame/Core.swift
+++ SwiftFrame/Core.swift
@@ -15,6 +15,7 @@ public typealias CoeffectMap = StringMap
 public typealias EventHandler<A, S> = (S?, A) -> S
 public typealias EffectHandler = (Any) -> ()
 public typealias CoeffectHandler = (CoeffectMap) -> CoeffectMap
+public typealias LoggingFunction = (String) -> ()
 
 public protocol Action {
     static var name: String { get }
@@ -34,7 +35,7 @@ public struct Interceptor {
     let after: ((Context) -> Context)?
 }
 
-public class Store<S> {
+public class Store<S: Equatable> {
     let registry: Registry
     var state: S
 
@@ -107,7 +108,7 @@ public class Store<S> {
     /// Lookup a coeffect handler and wrap it in a before Interceptor
     public func injectCoeffect(name: String) -> Interceptor {
         if let handler = registry.coeffectHandler(key: name) {
-            return Interceptor(name: "coeffect: \(name)", before: { (context: Context) in
+            return Interceptor(name: "coeffect: \(name)", before: { context in
                 var ctx = context
                 ctx.coeffects = handler(context.coeffects)
                 return ctx
@@ -119,7 +120,7 @@ public class Store<S> {
 
     /// An interceptor that does all effects in the effects map by calling registered `EffectHandler`s
     public func doEffects() -> Interceptor {
-        return Interceptor(name: "doEffects", before: nil, after: { (context: Context) in
+        return Interceptor(name: "doEffects", before: nil, after: { context in
             let effects = context.effects
             for (key, value) in effects {
                 if let handler = self.registry.effectHandler(key: key) {
@@ -134,6 +135,43 @@ public class Store<S> {
 
     public func injectState() -> Interceptor {
         return injectCoeffect(name: "state")
+    }
+
+    public var debug: Interceptor {
+        return debug { (str: String) in print(str) }
+    }
+
+    /// An interceptor that logs actions as they are processed and 
+    /// indicates changes to the state made by the event handler
+    public func debug(logFunction: @escaping LoggingFunction) -> Interceptor {
+        return Interceptor(name: "debug", before: { context in
+            if let action = context.coeffects["action"] {
+                logFunction("\nHandling action: \(action):")
+            }
+
+            return context
+        }, after: { context in
+            let logAfter = { (str: String) in logFunction("  \(str)") }
+
+            let action = context.coeffects["action"]
+            let oldState = context.coeffects["state"] as? S
+            let newState = context.effects["state"] as? S
+
+            if let old = oldState, let new = newState {
+                if old == new {
+                    logAfter("No state changes made by event handler for action: \(action)")
+                } else {
+                    logAfter("Old State: \(old)")
+                    logAfter("New State: \(new)")
+                }
+            } else {
+                logAfter("No state changes made by event handler for action: \(action)")
+            }
+
+            logAfter("")
+
+            return context
+        })
     }
 
     // Wrapper Interceptors
--- SwiftFrameTests/SwiftFrameTests.swift
+++ SwiftFrameTests/SwiftFrameTests.swift
@@ -9,29 +9,26 @@
 import XCTest
 @testable import SwiftFrame
 
+// Todos
+
+struct AddTodo: Action {
+    static var name = "AddTodo"
+    let name: String
+}
+
+struct AppState {
+    var todos: [String] = []
+}
+extension AppState: Equatable {}
+
+func ==(lhs: AppState, rhs: AppState) -> Bool {
+    return lhs.todos == rhs.todos
+}
+
 class SwiftFrameTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testTodoExample() {
-        struct AddTodo: Action {
-            static var name = "AddTodo"
-            let name: String
-        }
-
-        struct AppState {
-            var todos: [String] = []
-        }
-
         let store = Store(initialState: AppState())
-        store.registerEventState(actionClass: AddTodo.self) { (state, action) in
+        store.registerEventState(actionClass: AddTodo.self, interceptors: [store.debug]) { (state, action) in
             var s = state ?? AppState()
             s.todos.append(action.name)
             return s
