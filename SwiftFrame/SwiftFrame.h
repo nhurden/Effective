af917f5a82d4d6d7d674532161d36e27d18bedb9
Nicholas Hurden <git@nhurden.com>
2016-10-03 12:19:15 +1100

    Initial Commit

---
+++ SwiftFrame/SwiftFrame.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFrame.h
+//  SwiftFrame
+//
+//  Created by Nicholas Hurden on 3/10/2016.
+//  Copyright © 2016 Nicholas Hurden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFrame.
+FOUNDATION_EXPORT double SwiftFrameVersionNumber;
+
+//! Project version string for SwiftFrame.
+FOUNDATION_EXPORT const unsigned char SwiftFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFrame/PublicHeader.h>
+
+
